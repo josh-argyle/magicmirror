0c91b8ebaf745f3191543a105170579f41a5918f
Karsten Hassel <hassel@gmx.de>
2025-06-02 20:40:32 +0200

    update snyk image, get_package_files.sh only one package.json

--- debug/get_package_files.sh
+++ debug/get_package_files.sh
@@ -4,9 +4,3 @@ set -e
 
 docker cp mm:/opt/magic_mirror/package.json /mnt/c/data/repo/foreign/MagicMirror/
 docker cp mm:/opt/magic_mirror/package-lock.json /mnt/c/data/repo/foreign/MagicMirror/
-
-docker cp mm:/opt/magic_mirror/vendor/package.json /mnt/c/data/repo/foreign/MagicMirror/vendor/
-docker cp mm:/opt/magic_mirror/vendor/package-lock.json /mnt/c/data/repo/foreign/MagicMirror/vendor/
-
-docker cp mm:/opt/magic_mirror/fonts/package.json /mnt/c/data/repo/foreign/MagicMirror/fonts/
-docker cp mm:/opt/magic_mirror/fonts/package-lock.json /mnt/c/data/repo/foreign/MagicMirror/fonts/
